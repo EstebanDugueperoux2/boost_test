8eb79abee2e875152f7b8c1bb63f92410ea594d1
Esteban Dugueperoux <43169544+EstebanDugueperoux2@users.noreply.github.com>
2023-01-26 10:39:50 +0000

    Test boost cross compile with conan

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # boost_test
+
+conan new boost_test/0.0.1 --template=cmake_exe
+
+NOTE: Official Conan Docker images provides a arm tool chain (crossbuild-essential-armhf) too old to make cross compile of boost
+#docker run --rm -ti -v ${PWD}:/home/conan/project conanio/gcc8-ubuntu18.04
+#sudo apt -y update && sudo apt install -y crossbuild-essential-armhf
+#cd project
+
+docker run --rm -ti -v ${PWD}:/home/conan/project debian:bullseye
+apt -y update && apt install -y crossbuild-essential-armhf python3-pip cmake
+pip3 install conan
+cd /home/conan/project/
+
+conan create . --profile:build .conan/profiles/gcc10 --profile:host .conan/profiles/gcc10 --build missing  -c tools.system.package_manager:mode=install -c tools.system.package_manager:sudo=True
+
+conan create . --profile:build .conan/profiles/gcc10 --profile:host .conan/profiles/linux-armv7hf-gcc10 --build missing  -c tools.system.package_manager:mode=install -c tools.system.package_manager:sudo=True
---
+++ conanfile.py
@@ -0,0 +1,42 @@
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMakeDeps, CMake, cmake_layout
+
+
+class BoostTestConan(ConanFile):
+    name = "boost_test"
+    version = "0.0.1"
+
+    # Optional metadata
+    license = "<Put the package license here>"
+    author = "<Put your name here> <And your email here>"
+    url = "<Package recipe repository url here, for issues about the package>"
+    description = "<Description of BoostTest here>"
+    topics = ("<Put some tag here>", "<here>", "<and here>")
+
+    # Binary configuration
+    settings = "os", "compiler", "build_type", "arch"
+
+    # Sources are located in the same place as this recipe, copy them to the recipe
+    exports_sources = "CMakeLists.txt", "src/*", "include/*"
+
+    def requirements(self):
+        self.requires("boost/1.81.0")
+
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generate()
+
+        deps = CMakeDeps(self)
+        deps.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
